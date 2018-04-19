36478f51a2f162c87593aa881bf68dc8c2b9a74a
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-04-19 08:43:08 +0300

    lesson 02 and 03, table view fully functional and collection just the UI

---
+++ coder-swag/coder-swag/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by Sebastian Horatiu on 14/04/2018.
+//  Copyright © 2018 Sebastian Horatiu. All rights reserved.
+//
+
+import Foundation
---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Sebastian Horatiu on 19/04/2018.
+//  Copyright © 2018 Sebastian Horatiu. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
